a5b6c2f38e6f2b44d22b00d2f139922a79b708a0
Blake Eriks <eblake98@vt.edu>
2021-11-09 15:18:08 -1000

    Adding mobile functionality and movement structure

--- src/components/LogBook.js
+++ src/components/LogBook.js
@@ -10,55 +10,43 @@ const LogBook = () => {
     const [screenWidth, screenHeight] = useViewport()
     const [selectedDate, setSelectedDate] = useState(new Date())
     const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth())
-    const [liftsThisMonth, setLiftsThisMonth] = useState({})
+    const [movements, setMovements] = useState([])
     const [mobileMode, setMobileMode] = useState('view')
-
-    const addMovementToSelectedDay = movement => {
-        let newLiftsThisMonth = {...liftsThisMonth}, date = selectedDate.getDate()
-        if (!newLiftsThisMonth[date]) newLiftsThisMonth[date] = []
-        newLiftsThisMonth[date].push(movement)
-        setLiftsThisMonth(newLiftsThisMonth)
-    }
 
     useEffect( () => {
         MovementService.getAllForMonth(selectedMonth).then( res => {
-            let liftsThisMonth = {}
-            res.data.forEach(movement => {
-                let date = new Date(movement.date).getDate()
-                if (!liftsThisMonth[date]) liftsThisMonth[date] = []
-                liftsThisMonth[date].push(movement)
-            })
-            setLiftsThisMonth({...liftsThisMonth})
+            setMovements(res.data)
         })
     }, [selectedMonth])
 
+    useEffect( () => {
+        setMobileMode('edit')
+    }, [selectedDate])
+
     const createMovement = movement => {
-        movement.date = selectedDate.toLocaleDateString()
         MovementService.create(movement).then(res => {
-            addMovementToSelectedDay(res.data)
+            setMovements([...movements, res.data])
         })
     }
 
     const deleteMovement = id => {
         MovementService.delete(id).then(res => {
-            const newLiftsThisMonth = {...liftsThisMonth}
-            if (newLiftsThisMonth[selectedDate.getDate()].length === 1) {
-                newLiftsThisMonth[selectedDate.getDate()] = undefined
-            }
-            else {
-                newLiftsThisMonth[selectedDate.getDate()] = [...newLiftsThisMonth[selectedDate.getDate()].filter(movement => movement._id !== id)]
-            }
-            setLiftsThisMonth(newLiftsThisMonth)
+            setMovements([...movements.filter(movement => movement._id !== id)])
         })
     }
 
-    const isMobile = () => screenWidth < screenHeight
+    const isMobile = () => screenWidth < 600
 
     const mobileView = () => {
         return (
             <main>
-                {(mobileMode === 'edit') && <LiftEditor selectedDate={selectedDate} movements={liftsThisMonth[selectedDate.getDate()]} createMovement={createMovement}deleteMovement={deleteMovement} /> }
-                {(mobileMode === 'view') && <Calendar lifts={liftsThisMonth} setSelectedDate={setSelectedDate} selectedDate={selectedDate} selectedMonth={selectedMonth} />}
+                {(mobileMode === 'edit') && 
+                <LiftEditor selectedDate={selectedDate} 
+                    movements={movements.filter(movement => new Date(movement.date).getDate() === selectedDate.getDate())} 
+                    createMovement={createMovement}
+                    deleteMovement={deleteMovement} 
+                    setMobileMode={setMobileMode} /> }
+                {(mobileMode === 'view') && <Calendar movements={movements} setSelectedDate={setSelectedDate} selectedDate={selectedDate} selectedMonth={selectedMonth} />}
             </main>
         )
     }
@@ -66,9 +54,12 @@ const LogBook = () => {
     const desktopView = () => {
         return (
             <main>
-                <LiftEditor selectedDate={selectedDate} movements={liftsThisMonth[selectedDate.getDate()]} createMovement={createMovement} deleteMovement={deleteMovement}/>
+                <LiftEditor selectedDate={selectedDate} 
+                    movements={movements.filter(movement => new Date(movement.date).getDate() === selectedDate.getDate())} 
+                    createMovement={createMovement} 
+                    deleteMovement={deleteMovement}/>
                 <VerticalDivider height='70%'/>
-                <Calendar lifts={liftsThisMonth} setSelectedDate={setSelectedDate} selectedDate={selectedDate} selectedMonth={selectedMonth}/>
+                <Calendar movements={movements} setSelectedDate={setSelectedDate} selectedDate={selectedDate} selectedMonth={selectedMonth}/>
             </main>
         )
     }
